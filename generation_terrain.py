31514e935dd742b23d2b1038dd332b330b271b8d
Pierre COUCHENEY <pierre.coucheney@uvsq.fr>
2022-03-18 13:06:43 +0100

    implémentation de l'automate

--- generation_terrain.py
+++ generation_terrain.py
@@ -17,7 +17,7 @@ HAUTEUR = 600
 # largeur du canevas
 LARGEUR = 600
 # taille de la grille
-N = 4
+N = 50
 
 # choix des couleurs
 COUL_MUR = "grey"
@@ -26,6 +26,12 @@ COUL_VIDE = "white"
 # paramètres de l'automate
 # probabilité d'avoir un mur à l'initialisation
 P = 0.5
+# nombre d'itérations de l'automate
+NB_ITER = 1
+# seuil à partir duquel une case devient un mur
+SEUIL = 4
+# distance du voisinage
+D = 1
 
 
 ######################
@@ -113,6 +119,61 @@ def load():
     affiche_terrain()
 
 
+def compte_mur(i, j, d):
+    """Retourne le nombre de murs autour de la case de
+     coordonnées (i, j) à distance inférieure à d"""
+    cpt = 0
+    delta_i = 0
+    if i + d + 1 > N:
+        delta_i = N
+    for k in range(i-d-delta_i, i+d+1-delta_i):
+        delta_j = 0
+        if j + d + 1 > N:
+            delta_j = N
+        for l in range(j-d-delta_j, j+d+1-delta_j):
+            if terrain[k][l] == 1:
+                cpt += 1
+    if terrain[i][j] == 1:
+        cpt -= 1
+    return cpt
+
+
+
+def etape():
+    """fait une étape de l'automate"""
+    global terrain
+    terrain_res = []
+    for i in range(N):
+        terrain_res.append([0]*N)
+    for i in range(N):
+        for j in range(N):
+            mur = compte_mur(i, j, D)
+            if mur > SEUIL:
+                terrain_res[i][j] = 1
+            else:
+                terrain_res[i][j] = 0
+    terrain = terrain_res
+    affiche_terrain()
+
+
+
+def genere():
+    """générer un terrain"""
+    init_terrain()
+    for i in range(NB_ITER):
+        etape()
+
+
+def test_mur(event):
+    """Fonction qui affiche le nombre de mur autour de la case cliquée"""
+    x = event.x
+    y = event.y
+    largeur_case = LARGEUR // N
+    hauteur_case = HAUTEUR // N
+    i, j = x // largeur_case, y // hauteur_case
+    print("i=", i, "j=", j, compte_mur(i, j, D))
+
+
 
 #######################
 # programme principal
@@ -123,14 +184,16 @@ racine.title("Génération de terrain")
 canvas = tk.Canvas(racine, width=LARGEUR, height=HAUTEUR, bg="blue")
 bouton_sauv = tk.Button(racine, text="Sauvegarde", command=sauvegarde)
 bouton_load = tk.Button(racine, text="Charger terrain", command=load)
+bouton_genere = tk.Button(racine, text="génère terrain", command=genere)
 
 # placement des widgets
 canvas.grid(column=1, row=0, rowspan=10)
 bouton_sauv.grid(row=0)
 bouton_load.grid(row=1)
+bouton_genere.grid(row=2)
 
 
-init_terrain()
+canvas.bind("<Button-1>", test_mur)
 
 # boucle principale
 racine.mainloop()
